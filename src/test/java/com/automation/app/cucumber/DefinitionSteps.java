cfbcf6314d7f0af68a27a1b7cc63f23dbfbe1f1b
ashishraval007 <Ashishkumar@ashishraval007>
2015-08-08 16:00:05 -0400

    first commit

---
+++ src/test/java/com/automation/app/cucumber/DefinitionSteps.java
@@ -0,0 +1,32 @@
+package com.automation.app.cucumber;
+
+import net.thucydides.core.annotations.Steps;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import com.automation.app.steps.EndUserSteps;
+
+public class DefinitionSteps {
+
+    @Steps
+    EndUserSteps endUser;
+
+    @Given("the user is on the Wikionary home page")
+    public void givenTheUserIsOnTheWikionaryHomePage() {
+    
+    	endUser.openURL("http://en.wiktionary.org/wiki/Wiktionary");
+        endUser.is_the_home_page();
+    }
+
+    @When("the user looks up the definition of the word '(.*)'")
+    public void whenTheUserLooksUpTheDefinitionOf(String word) {
+        endUser.looks_for(word);
+    }
+
+    @Then("they should see the definition '(.*)'")
+    public void thenTheyShouldSeeADefinitionContainingTheWords(String definition) {
+        endUser.should_see_definition(definition);
+    }
+
+}
